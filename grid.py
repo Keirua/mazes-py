868b881305c869fa79674ae51f6a1ed59f78857c
clemkeirua <clement@keiruaprod.fr>
2019-01-20 16:24:14 +0100

    :recycle: extracted formatters

--- grid.py
+++ grid.py
@@ -1,5 +1,4 @@
 import random
-from PIL import Image, ImageDraw
 
 
 class Distances:
@@ -110,40 +109,3 @@ class Grid:
     def size(self):
         """The size of the grid"""
         return self.rows * self.columns
-
-    def __str__(self):
-        output = "+" + "---+" * self.rows + "\n"
-        for row in self.each_rows():
-            top = "|"
-            bottom = "+"
-            for cell in row:
-                body = " " * 3
-                east_boundary = " " if cell.has_link(cell.east) else "|"
-                top += body + east_boundary
-                south_boundary = " " * 3 if cell.has_link(cell.south) else "-" * 3
-                bottom += south_boundary + "+"
-            output += top + "\n"
-            output += bottom + "\n"
-        return output
-
-    def save_image(self, filename, cell_size=10):
-        wall_color = (0, 0, 0)
-        background_color = (255, 255, 255)
-
-        im = Image.new("RGB", (1 + self.rows * cell_size, 1 + self.columns * cell_size), background_color)
-        draw = ImageDraw.Draw(im)
-        for cell in self.each_cell():
-            x1 = cell.column * cell_size
-            y1 = cell.row * cell_size
-            x2 = (cell.column + 1) * cell_size
-            y2 = (cell.row + 1) * cell_size
-            if not cell.north:
-                draw.line((x1, y1, x2, y1), fill=wall_color)
-            if not cell.west:
-                draw.line((x1, y1, x1, y2), fill=wall_color)
-            if not cell.has_link(cell.east):
-                draw.line((x2, y1, x2, y2), fill=wall_color)
-            if not cell.has_link(cell.south):
-                draw.line((x1, y2, x2, y2), fill=wall_color)
-
-        im.save(filename, "PNG")
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from formatter import ImageFormatter, StringFormatter
 from grid import Grid
 from maze_generation import SideWinder
 
@@ -7,5 +8,6 @@ if __name__ == '__main__':
     g = Grid(10, 10)
 
     maze_generation_algorithm.apply_to(g)
-    print(g)
-    g.save_image("maze.png", 30)
+
+    print(StringFormatter.to_string(g))
+    ImageFormatter.save_image(g, "maze.png", 30)
